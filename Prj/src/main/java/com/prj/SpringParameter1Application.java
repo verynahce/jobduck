edd730aa5dc21efa2c2a3778e5eb147dec555fd3
Jung-YeonSu <wjddustn07@gmail.com>
2024-10-21 11:57:16 +0900

    home.jsp

---
+++ Prj/src/main/java/com/prj/SpringParameter1Application.java
@@ -0,0 +1,13 @@
+package com.prj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringParameter1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringParameter1Application.class, args);
+	}
+
+}
